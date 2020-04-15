27f5fc0b3639825422351462fc786ea7d73e4c5a
josh91hickman <josh91hickman@gmail.com>
2020-04-15 16:22:07 -0700

    cleanup + prep for prod

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ from setuptools import setup
 setup(
     name="tap-pendo",
     version="0.1.0",
-    description="Singer.io tap for extracting data",
-    author="Stitch",
-    url="https://app.pendo.io/api/v1/report",
+    description="Singer.io tap for extracting NPS data from the Pendo API",
+    author="Soxhub",
+    url="https://github.com/soxhub",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_pendo"],
     install_requires=[
         "singer-python>=5.0.12",
         "requests",
     ],
-    entry_points="""
-    [console_scripts]
-    tap-pendo=tap_pendo:main
-    """,
+    entry_points='''
+      [console_scripts]
+      tap-pendo=tap_pendo:main
+    ''',
     packages=["tap_pendo"],
-    package_data = {
-        "schemas": ["tap_pendo/schemas/*.json"]
+    package_data={
+      "schemas": ["schemas/*.json"]
     },
     include_package_data=True,
 )
